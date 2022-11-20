6aed8cd33aeec7c1ee8095cdb1018e2258d64488
kswlkswl <63973645+zxlzhangxiaolan@users.noreply.github.com>
2022-11-20 12:53:10 +0800

    change path

--- source_code/ssd-mobilenetv2/tools/train.py
+++ source_code/ssd-mobilenetv2/tools/train.py
@@ -24,8 +24,8 @@ from mmdet.utils import (collect_env, get_device, get_root_logger,
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Train a detector')
-    parser.add_argument('--config',default='./mmdetection/configs/ssd/ssdlite_mobilenetv2_scratch_600e_coco.py',help='train config file path')
-    parser.add_argument('--work-dir',default='./autodl-tmp/train_mask', help='the dir to save logs and models')
+    parser.add_argument('--config',default='./configs/ssd/ssdlite_mobilenetv2_scratch_600e_coco.py',help='train config file path')
+    parser.add_argument('--work-dir',default='./train_mask', help='the dir to save logs and models')
     parser.add_argument(
         '--resume-from', help='the checkpoint file to resume from')
     parser.add_argument(
